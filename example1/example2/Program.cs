d04a89f5a04fbab64682fd5b31b332b94200a0b5
29Tatiana <amiravob@mail.ru>
2022-08-31 10:50:50 +0300

    example2

---
+++ example1/example2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите 2 числа");
+string firstnumber = Console.ReadLine();
+string secondnumber = Console.ReadLine();
+var number1 = double.Parse(firstnumber);
+var number2 = double.Parse(secondnumber);
+if (number1 * number1 == number2)
+{ Console.WriteLine("да");
+}
+else
+{ Console.WriteLine("нет");
+}
